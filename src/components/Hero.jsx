0cd99b78eadc5c29eeae81876ff1dbdb49b7e9fe
Aviral Dhingra <deathblade287@192.168.1.8>
2024-08-15 20:26:19 +0530

    Update...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,22 @@
-
 import { styles } from "../styles";
 
 const Hero = () => {
   return (
     <section className="flex items-center justify-center w-full h-screen">
       <div className="text-center">
-        <h1 className={`text-white text-4xl md:text-6xl ${styles.heroHeadText}`}>
+        <h1
+          className={`text-white text-4xl md:text-6xl ${styles.heroHeadText}`}
+        >
           Hi, I'm <span className="text-[#915EFF]">Aviral</span>
         </h1>
-        <p className={`mt-4 text-white-100 text-xl md:text-2xl ${styles.heroSubText}`}>
-          Machine Learning Engineer, Systems Architect, Linux Enthusiast, Fitness Freak & An Overall Nerd...
+        <p
+          className={`mt-4 text-white-100 text-xl md:text-2xl ${styles.heroSubText}`}
+        >
+          Student By Obligation, Developer By Passion
           {/* I Develop ML Models, Automation Software & Web Apps<br /> */}
         </p>
       </div>
     </section>
-
 
     // <section className={`relative w-full h-screen mx-auto`}>
     //   <div
